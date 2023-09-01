015e484da5d0ed5d0944af81004c0b8865ae420c
Ion Stici <ionsticidev@gmail.com>
2023-09-01 17:44:01 +0300

    Restructure

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,36 +7,62 @@ import close from "./../assets/shared/mobile/close.svg";
 const Nav = function () {
   const [navStatus, setNavStatus] = React.useState(false);
 
-  const toggleNav = (e) => {
-    console.log(e);
+  const toggleNav = ({ target }) => {
+    const icon = target.querySelector("img");
+
+    if (!navStatus) {
+      // Open Nav
+
+      icon.src = close;
+
+      setNavStatus(true);
+      return;
+    }
+
+    if (navStatus) {
+      // Close Nav
+
+      icon.src = menu;
+
+      setNavStatus(false);
+      return;
+    }
   };
 
   return (
-    <nav className={styles.nav}>
-      <a className={styles.logo_link} href=".">
-        <img src={logo} alt="" />
-      </a>
+    <>
+      <div className={styles.container}>
+        <div className={styles.inner_container}>
+          <a className={styles.btn_logo} href=".">
+            <img src={logo} alt="" />
+          </a>
 
-      <button className={styles.btn_toggle_nav} onClick={toggleNav}>
-        <img src={close} alt="" />
-      </button>
+          <button className={styles.btn_toggle_nav} onClick={toggleNav}>
+            <img src={menu} alt="" />
+          </button>
 
-      <div className={styles.box}>
-        <ul className={styles.ul}>
-          <li>
-            <a href=".">Pricing</a>
-          </li>
-          <li>
-            <a href=".">About</a>
-          </li>
-          <li>
-            <a href=".">Contact</a>
-          </li>
-        </ul>
+          <div className={styles.nav_container}>
+            <div className={styles.nav_container_inner}>
+              <nav className={styles.nav}>
+                <ul>
+                  <li>
+                    <a href=".">Pricing</a>
+                  </li>
+                  <li>
+                    <a href=".">About</a>
+                  </li>
+                  <li>
+                    <a href=".">Contact</a>
+                  </li>
+                </ul>
+              </nav>
 
-        <button>Schedule a Demo</button>
+              <button className={styles.btn_cta}>Schedule a Demo</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
+    </>
   );
 };
 
